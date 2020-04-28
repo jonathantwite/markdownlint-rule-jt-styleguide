bc1cfca7692e085bcb0c6dce9a9de280f9ac7968
jonathantwite <13119611+jonathantwite@users.noreply.github.com>
2020-04-28 12:25:58 +0100

    feat(jt01): JT01 - sentence-start-capitalisation

--- rules.js
+++ rules.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const rules = [
-  //require("./rules/jt01"),
+  require("./rules/jt01"),
   //require("./rules/jt02"),
   //require("./rules/jt03"),
   require("./rules/jt04"),
---
+++ tests/jt01.spec.js
@@ -0,0 +1,56 @@
+const runmdl = require("./runMarkdownlint");
+const getErrors = require("./getLintErrors");
+
+const cases = [
+    ["# Title", 0],
+    ["# title", 1],
+    ["Title", 0],
+    ["title", 1],
+    ["Title.  Title", 0],
+    ["Title.  title", 1],
+    ["title.  Title", 1],
+    ["title.  title", 2],
+    ["Title.\n\ntitle", 1],
+    ["Title\n\ntitle", 1],
+];
+
+errLineCases = [
+    ["# title", 1],
+    ["title", 1],
+    ["Title.  title", 1],
+    ["title.  Title", 1],
+    ["title.  title", 1],
+    ["Title.\n\ntitle", 3],
+    ["Title\n\ntitle", 3],
+];
+
+errColCases = [
+    ["# title", 3],
+    ["title", 1],
+    ["Title.  title", 9],
+    ["title.  Title", 1],
+    ["Title.\n\ntitle", 1],
+    ["Title\n\ntitle", 1],
+];
+
+describe("JT01 - sentence-start-capitalisation", () => {
+    test.each(cases)("\"%s\" should generate %i errors", (str, expected) => {
+        const results = runmdl(str);
+        const forError = getErrors('JT01', results);
+        expect(forError.length).toBe(expected);
+    });
+
+    test.each(errLineCases)("\"%s\" should generate error on line %i", (str, expected) => {
+        const results = runmdl(str);
+        const error = getErrors('JT01', results)[0];
+        expect(error.lineNumber).toBe(expected);
+        expect(error.fixInfo.lineNumber).toBe(expected);
+    });
+
+    test.each(errColCases)("\"%s\" should generate error on column %i", (str, expected) => {
+        const results = runmdl(str);
+        const error = getErrors('JT01', results)[0];
+        expect(error.errorRange[0]).toBe(expected);
+        expect(error.fixInfo.editColumn).toBe(expected);
+    });
+});
